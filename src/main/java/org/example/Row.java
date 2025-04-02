5e573f8fe5daaa644e653790069dc922d96aeef0
Indirapriyadarshini Arunachalam <priya.a2105@gmail.com>
2025-04-02 15:01:26 -0400

    Update Row.java

--- src/main/java/org/example/Row.java
+++ src/main/java/org/example/Row.java
@@ -1,10 +1,13 @@
 package org.example;
 
-import java.nio.charset.StandardCharsets;
+//import java.nio.charset.StandardCharsets;
 
+//Row
 public class Row {
     // Define primary data type fields, depending on the schema of the table
     // These fields are for the Movies table described below
+
+    public static final int ROW_SIZE=39;
     public byte[] movieId;
     public byte[] title;
 
@@ -13,18 +16,15 @@ public class Row {
         this.title = title;
     }
 
-    // Converts the Row to a byte array for serialization or storage
+    //Converts the row data into a byte array
     public byte[] getBytes() {
-        // Calculate the total length of the combined byte array
         int totalLength = movieId.length + title.length;
 
-        // Create a new byte array to hold the combined data
         byte[] rowData = new byte[totalLength];
 
-        // Copy the movieId and title into the rowData byte array
         System.arraycopy(movieId, 0, rowData, 0, movieId.length);
         System.arraycopy(title, 0, rowData, movieId.length, title.length);
 
-        return rowData; // Return the byte array containing both movieId and title
+        return rowData;
     }
 }
