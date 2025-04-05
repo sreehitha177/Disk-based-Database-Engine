99d8829a800100b84c01c18e1f6f232b0226e612
Indirapriyadarshini Arunachalam <priya.a2105@gmail.com>
2025-04-05 17:26:55 -0400

    Add files via upload

---
+++ src/main/java/org/example/Rid.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Rid {
+    private final int pid;
+    private final int sid;
+
+    public Rid(int pid, int sid) {
+        this.pid = pid;
+        this.sid = sid;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+}
