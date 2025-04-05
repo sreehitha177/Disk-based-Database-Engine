ecccdf75931c55322f28a8f43c31f1e4ffd1bc32
Indirapriyadarshini Arunachalam <priya.a2105@gmail.com>
2025-04-05 17:24:36 -0400

    Update BufferManager.java

--- src/main/java/org/example/BufferManager.java
+++ src/main/java/org/example/BufferManager.java
@@ -1,39 +1,10 @@
 package org.example;
 
-//BufferManager
-public abstract class BufferManager {
-    //Configurable size of the buffer cache
-    final int bufferSize;
-
-    public BufferManager(int bufferSize) {
-        this.bufferSize = bufferSize;
-    }
-
-    /**
-     * Fetches a page from memory if available; otherwise, loads it from disk.
-     * The page is immediately pinned.
-     * @param pageId The ID of the page to fetch.
-     * @return The Page object whose content is stored in a frame of the buffer pool manager.
-     */
-    public abstract Page getPage(int pageId);
-
-    /**
-     * Creates a new page.
-     * The page is immediately pinned.
-     * @return The Page object whose content is stored in a frame of the buffer pool manager.
-     */
-    public abstract Page createPage();
-
-    /**
-     * Marks a page as dirty, indicating it needs to be written to disk before eviction.
-     * @param pageId The ID of the page to mark as dirty.
-     */
-    public abstract void markDirty(int pageId);
-
-    /**
-     * Unpins a page in the buffer pool, allowing it to be evicted if necessary.
-     * @param pageId The ID of the page to unpin.
-     */
-    public abstract void unpinPage(int pageId);
-
+public interface BufferManager {
+    Page getPage(String filePath, int pageId);
+    Page createPage(String filePath);
+    void markDirty(String filePath, int pageId);
+    void unpinPage(String filePath, int pageId);
+    void force(String filePath);
+//    void flushAllPages(String filePath);
 }
